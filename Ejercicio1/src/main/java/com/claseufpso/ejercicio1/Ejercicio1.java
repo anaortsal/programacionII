44d763f093223f91cf3cb4eb1d03bbfffc2239fc
Ana Salcedo <alortegas@ufpso.edu.co>
2024-02-29 15:19:22 -0500

    Creacion de personajes

--- Ejercicio1/src/main/java/com/claseufpso/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/main/java/com/claseufpso/ejercicio1/Ejercicio1.java
@@ -1,16 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.claseufpso.ejercicio1;
 
-/**
- *
- * @author SCIS2PC04
- */
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Character homero = new Character("Homero", 39, 'M', "A la grande le puse cuca");
+        homero.saySomething();
+
+        Character bart = new Character("Bartolomeo", 10, 'M', "Ay caramba");
+        bart.saySomething();
+    }
+
+    static class Character {
+
+        String name;
+        int age;
+        char gender;
+        String phrase;
+
+        public Character(String name, int age, char gender, String phrase) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.phrase = phrase;
+        }
+
+        void saySomething() {
+            System.out.println(this.phrase);
+        }
+
     }
 }
